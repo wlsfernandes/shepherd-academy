a7ca9b2944da67a7f12a6793217f848d5fd6ef67
wlsfernandes <wlsfernandes@gmail.com>
2025-12-18 14:43:40 -0500

    chore: remove compiled view cache and harden gitignore for template

--- storage/framework/views/ffda7b322f5160bd02ed16e9f57cec46.php
+++
@@ -1,7 +0,0 @@
-<meta charset="utf-8" />
-<title><?php echo $__env->yieldContent('title'); ?> | devpromaster</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta content="devpromaster Administration Tool" name="description" />
-<meta content="devpromaster" name="author" />
-<!-- App favicon -->
-<link rel="shortcut icon" href="<?php echo e(asset('assets/admin/images/devpromaster_ico.png')); ?>"><?php /**PATH /var/www/html/complete_template_laravel_admin/resources/views/admin/layouts/title-meta.blade.php ENDPATH**/ ?>
